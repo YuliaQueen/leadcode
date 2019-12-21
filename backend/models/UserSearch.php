4c2a30078f588bb6bc9cf6799032e9499c08ece8
Yulia <youlka25@yandex.ru>
2019-12-21 21:07:46 +0300

    created UserSearch.php

---
+++ backend/models/UserSearch.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace backand\models;
+
+
+use common\models\User;
+use yii\data\ActiveDataProvider;
+
+class UserSearch extends User
+{
+    public function rules()
+    {
+        return [
+            [['username, email, status'], 'safe']
+        ];
+    }
+
+    public function search($params)
+    {
+        $query = static::find();
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email]);
+
+        return $dataProvider;
+    }
+
+}
